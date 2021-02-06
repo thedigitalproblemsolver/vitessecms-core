2252e936837a08063ffbc9e85c86abcc313ac3bf
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-02-06 16:42:50 +0100

    Moved DatagroupRepository.php

--- src/Module.php
+++ src/Module.php
@@ -4,7 +4,7 @@ namespace VitesseCms\Core;
 
 use VitesseCms\Block\Repositories\BlockRepository;
 use VitesseCms\Datafield\Repositories\DatafieldRepository;
-use VitesseCms\Core\Repositories\DatagroupRepository;
+use VitesseCms\Datagroup\Repositories\DatagroupRepository;
 use VitesseCms\Core\Repositories\JobQueueRepository;
 use VitesseCms\Core\Repositories\RepositoryCollection;
 use VitesseCms\User\Repositories\UserRepository;
