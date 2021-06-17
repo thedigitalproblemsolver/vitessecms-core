16a1cdfe5d0ed86025667052fcf3c44fa3d55909
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-06-17 19:08:39 +0200

    Removed FactoryInterface.php

--- src/Factories/ObjectFactory.php
+++ src/Factories/ObjectFactory.php
@@ -3,10 +3,9 @@
 namespace VitesseCms\Core\Factories;
 
 use VitesseCms\Core\Interfaces\BaseObjectInterface;
-use VitesseCms\Core\Interfaces\FactoryInterface;
 use VitesseCms\Core\Traits\BaseObjectTrait;
 
-class ObjectFactory implements FactoryInterface
+class ObjectFactory
 {
     public static function create(BaseObjectInterface $bindata = null): BaseObjectInterface
     {
