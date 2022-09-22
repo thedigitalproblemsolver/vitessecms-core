cc604a55f8ccc8777e3689e9e610f4561d01cd28
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2022-09-22 18:13:02 +0200

    Removed Phalcon uses

--- src/Enum/EnvEnum.php
+++ src/Enum/EnvEnum.php
@@ -9,4 +9,5 @@ class EnvEnum extends AbstractEnum
     public const CACHE_LIFE_TIME = 'CACHE_LIFE_TIME';
     public const ENVIRONMENT = 'ENVIRONMENT';
     public const ENVIRONMENT_LOCAL = 'local';
+    public const ENVIRONMENT_PRODUCTION = 'production';
 }
